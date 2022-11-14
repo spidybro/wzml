2f06a641c74224fb28186015376a22e8c3bd2491
spidybro <89572178+spidybro@users.noreply.github.com>
2022-11-14 08:40:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -194,7 +194,7 @@ def log(update, context):
 
 
 help_string = '''
-<b><a href='https://t.me/mr_spidybot'>Any Query</a></b> - The Ultimate Telegram MIrror-Leech Bot to Upload Your File & Link in Google Drive & Telegram
+<b><a href='https://t.me/Mr_SpidyBot'>Any Query</a></b> - The Ultimate Telegram MIrror-Leech Bot to Upload Your File & Link in Google Drive & Telegram
 Choose a help category:
 '''
 
@@ -282,9 +282,15 @@ help_string_telegraph_user = f'''
 • <b>/weebhelp</b>: Okatu helper
 '''
 
-help_user = telegraph.create_page(
-    title=f"{TITLE_NAME} Help",
-    content=help_string_telegraph_user)["path"]
+try:
+    help_user = telegraph.create_page(
+        title='{TITLE_NAME} HELP',
+        content=help_string_telegraph_user,
+    )["path"]
+except Exception as err:
+    LOGGER.warning(f"{err}")
+    pass
+
 
 help_string_telegraph_admin = f'''
 <b><u>🛡️ Admin Commands</u></b>
@@ -312,10 +318,14 @@ help_string_telegraph_admin = f'''
 • <b>/{BotCommands.LogCommand}</b>: Get a log file of the bot. Handy for getting crash reports
 '''
 
-help_admin = telegraph.create_page(
-    title=f"{TITLE_NAME} Help",
-    content=help_string_telegraph_admin)["path"]
-
+try:
+    help_admin = telegraph.create_page(
+        title='{TITLE_NAME} HELP',
+        content=help_string_telegraph_admin,
+    )["path"]
+except Exception as err:
+    LOGGER.warning(f"{err}")
+    pass
 
 def bot_help(update, context):
     button = ButtonMaker()
